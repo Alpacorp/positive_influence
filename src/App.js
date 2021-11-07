5bc4dc39069292c7f11d3cf09d5bd62ec10571e7
Alpacorp <alejandro.palacios88@gmail.com>
2021-11-06 22:19:36 -0500

    Implementation materia-table to users data with mocks and API real users

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Users from './pages/Users/Users';
 
 function App() {
   return (
     <div className="App">
+      <Users />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
