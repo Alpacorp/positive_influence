820946a981b52d18c5a92fda2a397471f39c0fc9
Alpacorp <alejandro.palacios88@gmail.com>
2021-11-15 22:15:32 -0500

    add new menu layout, best layout than before version.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Users from './pages/Users/Users';
+// import Users from './pages/Users/Users';
+import App from './App';
 
 ReactDOM.render(
-  <Users />,
+  // <Users />,
+  <App />,
   document.getElementById('root')
 );
