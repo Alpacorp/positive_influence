c6f2b00518ba703d558c50922af1a340be517542
Alpacorp <alejandro.palacios88@gmail.com>
2022-05-09 23:21:28 -0500

    add react redux library

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { MenuTab } from './Components/Menu/MenuTab';
+import React, { useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { MenuTab } from "./Components/Menu/MenuTab";
 
 const App = () => {
-
   useEffect(() => {
-    localStorage.removeItem('key');
+    localStorage.removeItem("key");
   }, []);
 
   return (
     <BrowserRouter>
       <MenuTab />
     </BrowserRouter>
-  )
+  );
 };
 
 export default App;
